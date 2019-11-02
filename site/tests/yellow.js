215e372249e785751c2d395332593b20738fe314
Alina <kolmykova93@gmail.com>
2019-11-02 23:06:04 +0700

    get names in loop

--- site/tests/yellow.js
+++ site/tests/yellow.js
@@ -1,15 +1,25 @@
-function checkAnswer(answers) {
-    const url = document.URL;
-    const data = document.getElementsByName("O-soto-otoshi");
+function checkAnswer() {
 
-    for (i = 0; i < data.length; i++) {
-        if (data[i].checked) {
-            rightAnswer = data[i].name;
-            choicedAnswer = data[i].value;
-            if (choicedAnswer == rightAnswer) {
-                alert("Well done!")
-            } else {
-                alert("Try again");
+    tecknicsYellow = ["O-soto-otoshi", "O-uchi-gari",
+        "O-goshi", "Mune-gatame", "Kuzure-kesa-gatame", "Ko-soto-gari",
+        "Ko-uchi-gari", "Hiza-guruma", "Eri-seoi-nage", "Koshi-guruma",
+        "Kami-shiho-gatame", "Tate-shiho-gatame"
+    ];
+
+    for (tecknicNumber = 0; tecknicNumber < tecknicsYellow.length; tecknicNumber++) {
+        tecknicName = tecknicsYellow[tecknicNumber];
+        radioButtonResult = document.getElementsByName(tecknicName);
+
+        for (i = 0; i < radioButtonResult.length; i++) {
+            if (radioButtonResult[i].checked) {
+                rightAnswer = radioButtonResult[i].name;
+                choicedAnswer = radioButtonResult[i].value;
+                console.log(tecknicsYellow[tecknicNumber]);
+                if (choicedAnswer == rightAnswer) {
+                    console.log("Well done!");
+                } else {
+                    console.log("Try again");
+                }
             }
         }
     }
