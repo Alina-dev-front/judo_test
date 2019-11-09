10fdd9e3199b755585528faaf92903085ec77d94
Alina <kolmykova93@gmail.com>
2019-11-10 05:04:39 +0700

    implement check answer for orange page

--- site/tests/Orange/orange.js
+++ site/tests/Orange/orange.js
@@ -1,19 +1,50 @@
     imagesLink = ["../Yellow/Yellow_img/Eri-seoi-nage.jpg",
         "../Yellow/Yellow_img/Hiza-guruma.jpg", "../Yellow/Yellow_img/Ko-soto-gari.jpg"
     ];
+    tecknicsOrange = ["tecknic1", "tecknic2", "tecknic3"];
 
-    myHTML = '';
-    var element = document.getElementById("fetch-images");
+    function generateTest() {
+        myHTML = '';
+        var element = document.getElementById("fetch-images");
 
-    for (i = 0; i < imagesLink.length; i++) {
-        myHTML += '<div class="container"><div id="image-id"><img src=' +
-            imagesLink[i] + ' class="test-image"></div>' +
-            '<div class="top-left">' +
-            '<input type="radio" name="O-soto-otoshi" value="O-soto-gari">O-soto-gari<br>' +
-            '<input type="radio" name="O-soto-otoshi" value="O-soto-otoshi">O-soto-otoshi<br>' +
-            '<input type="radio" name="O-soto-otoshi" value="O-uchi-gari">O-uchi-gari<br>' +
-            '</div></div>'
-        console.log(imagesLink[i]);
+        for (i = 0; i < imagesLink.length; i++) {
+            myHTML += '<div class="container"><div style="margin-left: 200px" id="image-id"><img src=' +
+                imagesLink[i] + ' class="test-image"></div>' +
+                '<div class="top-left">' +
+                '<input type="radio" name=' + tecknicsOrange[i] + ' value=' + tecknicsOrange[i] + '>' + tecknicsOrange[i] + '<br>' +
+                '<input type="radio" name=' + tecknicsOrange[i] + ' value="O-soto-otoshi">O-soto-otoshi<br>' +
+                '<input type="radio" name=' + tecknicsOrange[i] + ' value="O-uchi-gari">O-uchi-gari<br>' +
+                '</div></div>'
+        }
+        element.innerHTML = myHTML;
     }
-    console.log(element);
-    element.innerHTML = myHTML;
+
+    function checkAnswer() {
+        countRightAnswers = 0;
+        countWrongAnswers = 0;
+
+        for (var tecknicNumber = 0; tecknicNumber < tecknicsOrange.length; tecknicNumber++) {
+            tecknicName = tecknicsOrange[tecknicNumber];
+            radioButtonResult = document.getElementsByName(tecknicName);
+
+            for (i = 0; i < radioButtonResult.length; i++) {
+                if (radioButtonResult[i].checked) {
+                    if (radioButtonResult[i].value == radioButtonResult[i].name) {
+                        countRightAnswers++;
+                    } else {
+                        countWrongAnswers++;
+                    }
+                }
+            }
+        }
+        document.getElementById("orangeresult").style.display = "block";
+        document.getElementById("right_answers").innerHTML = countRightAnswers;
+        document.getElementById("wrong_answers").innerHTML = countWrongAnswers;
+
+        this.scrollDown();
+    }
+
+    function scrollDown() {
+        resultElement = document.getElementById("orangeresult");
+        resultElement.scrollIntoView();
+    }
